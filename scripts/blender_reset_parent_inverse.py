4aba780c2fd06b4c97992be14dca62fde668d4c0
Max Goodman <c@chromakode.com>
2017-08-19 21:44:47 -0700

    Add Blender operation script for resetting parent inverse matrix

---
+++ scripts/blender_reset_parent_inverse.py
@@ -0,0 +1,18 @@
+import bpy
+
+
+class ResetParentInverse(bpy.types.Operator):
+    """Resets an object's parent_inverse_matrix, retaining transform."""
+    bl_idname = "chromakode.reset_parent_inverse"
+    bl_label = "Reset Parent Inverse"
+
+    def execute(self, context):
+        # via https://blender.stackexchange.com/a/28897/41041
+        obj = context.object
+        matrix_orig = obj.matrix_world.copy()
+        obj.matrix_parent_inverse.identity()
+        obj.matrix_basis = obj.parent.matrix_world.inverted() * matrix_orig
+        return {'FINISHED'}
+
+
+bpy.utils.register_class(ResetParentInverse)
